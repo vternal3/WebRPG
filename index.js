37410a213234b4c848e7d0d1ef402fcc258b0585
Jeb <jbradwell0809@gmail.com>
2018-10-03 16:57:50 -0700

    Added socket.io.js updated some css set index.js to use index.html instead of client.html

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express(); 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//Will need to put web inside a 'public' directory
+//instead of socket.io being located in the root.
+app.use(express.static(__dirname + '/'));
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/client.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
