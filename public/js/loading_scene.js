09e4b40432d8c58677748e51b1b3d7bed87dfcd7
Vternal3 <vternal3@gmail.com>
2018-11-10 13:55:10 -0800

    reorganized the asset hierarchy

--- public/js/loading_scene.js
+++ public/js/loading_scene.js
@@ -22,23 +22,23 @@ class loading_scene extends Phaser.Scene {
 		
 		this.load.image('star', 'assets/star_gold.png');
 		
-		this.load.image("tiles", "spritesheets/45a71fb70eda0e7a608f09bf4a13fd6a.png");
-		this.load.image("tiles2", "spritesheets/45a71fb70eda0e7a608f09bf4a13fd6a.png");
-		this.load.image("grass2", "spritesheets/grass-tile-3.png");
-		this.load.spritesheet("walk_template", "spritesheets/walk_template.png", { frameWidth: 60, frameHeight: 110});
+		this.load.image("tiles", "assets/spritesheets/45a71fb70eda0e7a608f09bf4a13fd6a.png");
+		this.load.image("tiles2", "assets/spritesheets/45a71fb70eda0e7a608f09bf4a13fd6a.png");
+		this.load.image("grass2", "assets/spritesheets/grass-tile-3.png");
+		this.load.spritesheet("walk_template", "assets/spritesheets/walk_template.png", { frameWidth: 60, frameHeight: 110});
 		
-		this.load.spritesheet("title_crystal", "spritesheets/crystal-qubodup-ccby3-32-blue.png", { frameWidth: 32, frameHeight: 32});
-		this.load.image("title_bg", "spritesheets/parallax-mountain-bg.png");
-		this.load.image("start", "spritesheets/start.png");
-		this.load.image("start_hover", "spritesheets/start_hover.png");
-		this.load.image("settings", "spritesheets/settings.png");
-		this.load.image("settings_hover", "spritesheets/settings_hover.png");
+		this.load.spritesheet("title_crystal", "assets/spritesheets/crystal-qubodup-ccby3-32-blue.png", { frameWidth: 32, frameHeight: 32});
+		this.load.image("title_bg", "assets/spritesheets/parallax-mountain-bg.png");
+		this.load.image("start", "assets/spritesheets/start.png");
+		this.load.image("start_hover", "assets/spritesheets/start_hover.png");
+		this.load.image("settings", "assets/spritesheets/settings.png");
+		this.load.image("settings_hover", "assets/spritesheets/settings_hover.png");
 		
-		this.load.image("ui_upscaled", "spritesheets/ui_upscaled.png");
+		this.load.image("ui_upscaled", "assets/spritesheets/ui_upscaled.png");
 		
-		this.load.tilemapTiledJSON("map3", "spritesheets/untitled3.json");
+		this.load.tilemapTiledJSON("map3", "assets/spritesheets/untitled3.json");
 		
-		this.load.audio('theme_music', 'music/Soliloquy.mp3');
+		this.load.audio('theme_music', 'assets/audio/music/Soliloquy.mp3');
 		
 		let loadingBar = this.add.graphics({
 			fillStyle: {
